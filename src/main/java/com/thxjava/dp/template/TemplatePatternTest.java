89c363e121c686b8ade835164089528874e84741
whatisjava <pursuemoon@qq.com>
2018-06-22 16:46:46 +0800

    template pattern

---
+++ src/main/java/com/thxjava/dp/template/TemplatePatternTest.java
@@ -0,0 +1,13 @@
+package com.thxjava.dp.template;
+
+public class TemplatePatternTest {
+
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+
+}
